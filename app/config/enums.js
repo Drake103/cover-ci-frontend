384196e928121ab995d7ace9b6b4327d198c40db
drake103 <agabden@gmail.com>
2015-12-11 01:14:19 +0600

    Some styling. Corrected json a bit. Processing status codes.

---
+++ app/config/enums.js
@@ -0,0 +1,11 @@
+var enums = {
+  BuildRunStatusesEnum: Object.freeze({
+    UNDEFINED: 0,
+    PENDING: 1,
+    RUNNING: 2,
+    PASSED: 3,
+    FAILED: 4,
+  }),
+};
+
+export default enums;
--- app/modules/buildRuns/buildRuns.controller.js
+++ app/modules/buildRuns/buildRuns.controller.js
@@ -1,4 +1,7 @@
 import _ from 'lodash';
+import enums from '../../config/enums';
+
+var BuildRunStatusesEnum = enums.BuildRunStatusesEnum;
 
 class BuildRunsController {
   constructor($scope, BuildRunsService) {
@@ -11,8 +14,46 @@ class BuildRunsController {
     });
   }
 
-  getStatusClass(buildRun) {
-    return 'cci-status-failed';
+  getStatusClassLi(statusCode) {
+    switch (statusCode) {
+      case BuildRunStatusesEnum.PENDING:
+        return 'cci-li-status-pending';
+      case BuildRunStatusesEnum.RUNNING:
+        return 'cci-li-status-running';
+      case BuildRunStatusesEnum.PASSED:
+        return 'cci-li-status-passed';
+      case BuildRunStatusesEnum.FAILED:
+        return 'cci-li-status-failed';
+    }
+    return 'cci-li-status-default';
+  }
+
+  getStatusIconClass(statusCode) {
+    switch (statusCode) {
+      case BuildRunStatusesEnum.PENDING:
+        return 'fa-dot-circle-o';
+      case BuildRunStatusesEnum.RUNNING:
+        return 'fa-refresh';
+      case BuildRunStatusesEnum.PASSED:
+        return 'fa-check-circle-o';
+      case BuildRunStatusesEnum.FAILED:
+        return 'fa-times-circle-o';
+    }
+    return null;
+  }
+
+  getStatusColorClass(statusCode) {
+    switch (statusCode) {
+      case BuildRunStatusesEnum.PENDING:
+        return 'cci-status-pending';
+      case BuildRunStatusesEnum.RUNNING:
+        return 'cci-status-running';
+      case BuildRunStatusesEnum.PASSED:
+        return 'cci-status-passed';
+      case BuildRunStatusesEnum.FAILED:
+        return 'cci-status-failed';
+    }
+    return 'cci-status-default';
   }
 
   expand(buildRun) {
